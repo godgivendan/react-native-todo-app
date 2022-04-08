9412dee7c3ff96763f2a0160c22ec248b1685b57
Denis Bogdanovskiy <bogdanovskiy.dan@gmail.com>
2022-04-08 13:08:56 +0500

    Todo remove

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View, ScrollView } from 'react-native';
+import { StyleSheet, View, FlatList } from 'react-native';
 import { Navbar } from "./src/components/Navbar";
 import { AddTodo } from "./src/components/AddTodo";
 import  { Todo } from "./src/components/Todo";
@@ -8,17 +8,6 @@ import  { Todo } from "./src/components/Todo";
 export default function App() {
   const [todos, setTodos] = useState([]);
   const addTodo = (title) => {
-      // const newTodo = {
-      //   id: Date.now().toString(),
-      //   title: title
-      // };
-      // setTodos(todos.concat([newTodo]));
-      // setTodos((prevTodos) => {
-      //   return [
-      //       ...prevTodos,
-      //       newTodo
-      //   ];
-      // });
       setTodos(prev => [
           {
               id: Date.now().toString(),
@@ -27,22 +16,21 @@ export default function App() {
           ...prev
       ]);
   }
+  const removeTodo = (id) => {
+      setTodos(todos.filter((todo) => todo.id !== id));
+  }
 
   return (
     <View style={styles.container}>
         <Navbar title="Todo App!"/>
         <View style={styles.content}>
             <AddTodo onSubmit={addTodo} btnTitle="Добавить" inputPlaceholder="Введите наименование задачи..."/>
-            <View style={styles.todoList}>
-                <ScrollView
-                    alwaysBounceVertical={true}
-                    contentContainerStyle={styles.todoListContent}
-                    >
-                    {todos.map(todo => {
-                        return <Todo todo={todo} key={todo.id} />
-                    })}
-                </ScrollView>
-            </View>
+            <FlatList
+                keyExtractor={item => item.id.toString()}
+                data={todos}
+                renderItem={({item}) => <Todo todo={item} onRemove={removeTodo} /> }
+                style={styles.todoList}
+            />
             <StatusBar style="auto" />
         </View>
     </View>
@@ -54,16 +42,13 @@ const styles = StyleSheet.create({
         flex: 1,
         // backgroundColor: '#000',
         // alignItems: 'center',
-        //justifyContent: 'center'
+        // justifyContent: 'center'
     },
     content: {
         paddingHorizontal: 30,
         paddingVertical: 20
     },
     todoList: {
-        height: '87%'
-    },
-    todoListContent: {
-        paddingVertical: 5
+        height: '80%'
     }
 });
